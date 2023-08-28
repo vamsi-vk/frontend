47987648825f0496affe272e4b2cc550172b23f2
venureddy759 <evgr759@gmail.com>
2023-08-29 00:24:45 +0530

    login and signup creeated

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -5,20 +5,19 @@ import { AppComponent } from './app.component';
 import { AdminModule } from './admin/admin.module';
 import { MetrialModule } from './metrial/metrial.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-
+import { LoginModule } from './login/login.module';
 @NgModule({
   declarations: [
     AppComponent,
-
   ],
   imports: [
+    
     BrowserModule,
     AppRoutingModule,
     MetrialModule,
     BrowserAnimationsModule,
     AdminModule,
-
+    LoginModule
 
   ],
   providers: [],
